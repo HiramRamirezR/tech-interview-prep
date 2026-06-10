411b2917f910527ca11cf4f4a01d89ac81ca2ef1
Hiram Ramírez <68158101+HiramRamirezR@users.noreply.github.com>
2026-06-10 12:48:28 -0600

    First standalone test with CI/CD pipeline

--- practice-python/ex-01-string-manipulation/solution.py
+++ practice-python/ex-01-string-manipulation/solution.py
@@ -1,1 +1,14 @@
-# Your solution here
+def camel_to_snake(input):
+    result = ""
+    for index, char in enumerate(input):
+        if index > 0:
+            if char.isupper():
+                result += "_" + char.lower()
+            else:
+                result += char
+        else:
+            result += char.lower()
+    
+    return result
+
+camel_to_snake("snake_case")
