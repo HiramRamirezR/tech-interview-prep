92ae55d3604fd3bb44fee1e04ee19b5e339c5823
Hiram Ramírez <68158101+HiramRamirezR@users.noreply.github.com>
2026-06-26 21:41:08 -0600

    ex-05-hash-map solution

--- practice-python/ex-05-hash-map/solution.py
+++ practice-python/ex-05-hash-map/solution.py
@@ -1,1 +1,8 @@
-# Your solution here
+def char_frequency(input):
+    result = {}
+    if input == "":
+        return {}
+    for i in input:
+        times = result.get(i, 0)
+        result[i] = times + 1 
+    return result
